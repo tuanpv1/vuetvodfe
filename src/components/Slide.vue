de2d8f59cc8fc21c181bc7ef94d102a4b043fbea
Tuan Pham <tuantrylook@gmail.com>
2018-07-02 10:29:14 +0700

    a

---
+++ src/components/Slide.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="slide">
+        <span v-html="title"></span>
+        <p>{{ user.first_name}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'slide',
+//        data () {
+//            return {
+//                title: '<h1>PHAM VAN TUAN</h1>',
+//                user: {
+//                    first_name: 'Tuan',
+//                    last_name: 'Van'
+//                }
+//            }
+//        }
+    }
+</script>
